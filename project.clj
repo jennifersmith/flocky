8ebc0c153a602be8695410519080e255ddcdcac0
Jennifer Smith <jennifer@home>
2013-10-17 09:46:30 +0100

    Something sane for brower repl stuff

--- project.clj
+++ project.clj
@@ -1,14 +1,32 @@
 (defproject flocky "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
-  :plugins [[lein-ring "0.8.5"]
-            [lein-cljsbuild "0.3.0"]]
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+                 [org.clojure/clojurescript "0.0-1909"]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.3"]
+                 [domina "1.0.2-SNAPSHOT"]
+                 [org.clojure/data.json "0.2.2"]
+                 [com.cemerick/piggieback "0.1.0"]
+                 ]
+  :source-paths ["src/clj"]
+
+  :plugins [[lein-cljsbuild "0.3.2"]
+            [lein-ring "0.8.5"]]
+
   :ring {:handler flocky.handler/app}
-  :profiles
-  {:dev {:dependencies [[ring-mock "0.1.5"]]}}
-  :cljsbuild {:builds [{:source-paths ["src/cljs"]
-                        :compiler {:output-to "resources/public/development.js"
+
+  :cljsbuild {:builds {:dev
+                       {:source-paths ["src/cljs"]
+                        :compiler {:output-to "resources/public/js/dev.js"
                                    :optimizations :whitespace
-                                   :pretty-print true}}]})
+                                   :pretty-print true}}
+
+                       :prod
+                       {:source-paths ["src/cljs"]
+                        :compiler {:output-to "resources/public/js/prod.js"
+                                   :optimizations :advanced}}}}
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]})
